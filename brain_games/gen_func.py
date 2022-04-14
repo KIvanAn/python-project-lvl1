87f5491f1206f15cd2cb571b54c4a91f5893eebd
Ivan <kozel.vania@yandex.ru>
2022-04-14 23:13:23 +0300

    Fix brain prime game 0.1.0

--- brain_games/gen_func.py
+++ brain_games/gen_func.py
@@ -117,6 +117,9 @@ def is_prime():
     number = randint(0, 100)
     print('Question: {0}'.format(number))
 
+    if number <= 1:
+        return 'no'
+
     for num in range(2, int(number**0.5) + 1):
         if number % num == 0:
             return 'no'
